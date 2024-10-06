6fe2ae6419eff09f160f3f49226e8ae9ceaba069
John Desmond <johndesmond631@gmail.com>
2024-10-06 12:15:08 -0400

    chanegd from typesript to js, and updated resolvers

---
+++ graphql-server-example/index.js
@@ -0,0 +1,46 @@
+import { ApolloServer } from '@apollo/server'; // preserve-line
+import { startStandaloneServer } from '@apollo/server/standalone'; // preserve-line
+
+import { typeDefs } from './schema.js';
+
+import db from './_db.js';
+
+const resolvers = {
+    Query: {
+        games() {
+            // just needs to point to where to grab the data from
+            // field selection is handled by graphql/graphql server
+            return db.games
+        },
+        reviews() {
+            return db.reviews 
+        },
+        authors() {
+            return db.authors
+        }
+    }
+};
+
+/*
+games {
+    title
+}
+*/
+
+
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  
+  // Passing an ApolloServer instance to the `startStandaloneServer` function:
+  //  1. creates an Express app
+  //  2. installs your ApolloServer instance as middleware
+  //  3. prepares your app to handle incoming requests
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+  });
+  
+  console.log(`🚀  Server ready at: ${url}`);
